c294957c8ee0619cd6378b97a29aff869a565f4d
mariscinis <maarisc@inbox.lv>
2022-05-30 20:06:54 +0300

    Update 7

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,22 @@
 $('[lang="en"]').hide();
 
+let imageHeight = $(".section-image").height();
+let difference = $(window).height() - imageHeight;
+
 $(window).on('load', function () {
+    if (difference >= 400) {
+        $(".section-background").height(difference);
+        $(".section-text-container").height(difference - 100);
+    }
+
     $("html, body").animate({ scrollTop: $(document).height() }, 30);
+    $(".intro>.arrow-container").fadeOut(150).fadeIn(150).fadeOut(150).fadeIn(150);
+});
+
+$(".arrow-container").on('click', function(event) {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(".image" + event.target.id).offset().top - difference
+    }, 1000);
 });
 
 $("#lv").click(function() {
