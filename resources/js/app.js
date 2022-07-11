98b272372d8a664cb85aaa3415ea0423a7e8a4e9
mariscinis <105968011+mariscinis@users.noreply.github.com>
2022-07-11 22:06:02 +0300

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,4 @@
-window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1");
 // $('[lang="en"]').hide();
 
 // let imageHeight;
